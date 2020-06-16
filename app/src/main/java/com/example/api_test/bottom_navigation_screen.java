cb8731aa109f6b2d9ccc92c6cd4489ec7d431dab
amoghamanju <amoghamanju@gmail.com>
2020-06-16 14:00:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/api_test/bottom_navigation_screen.java
@@ -0,0 +1,14 @@
+package com.example.api_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bottom_navigation_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation_screen);
+    }
+}
